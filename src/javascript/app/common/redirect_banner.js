91dac266ed8a2afbb44ff053507b85578cfe3920
Meenakshi <meenakshi@Meenakshis-MacBook-Pro.local>
2022-08-03 13:31:30 +0400

    redirect row users

--- src/javascript/app/common/redirect_banner.js
+++ src/javascript/app/common/redirect_banner.js
@@ -1,3 +1,4 @@
+const Cookies          = require('js-cookie');
 const DerivBanner = require('./deriv_banner');
 const BinarySocket = require('../base/socket');
 const State = require('../../_common/storage').State;
@@ -12,6 +13,8 @@ const RedirectBanner = (() => {
             const eu_country = isEuCountrySelected(Client.get('residence')) || isEuCountrySelected(State.getResponse('website_status.clients_country'));
             if (eu_country) {
                 handleRedirect();
+            } else {
+                handleRedirectROW();
             }
             
         });
@@ -20,6 +23,23 @@ const RedirectBanner = (() => {
 
     const handleRedirect = () => {
         window.location.href = '/move-to-deriv/';
+    
+    };
+    
+    const handleRedirectROW = () => {
+        
+        // Check if param have ?binary-com-lp
+        const redirectBinary = new URLSearchParams(window.location.search);
+        const date = new Date();
+        date.setTime(date.getTime()+(2*60*1000));
+       
+        if (redirectBinary.has('binary-com-lp') || Cookies.get('binary-com-show')){
+            // Set cookie if they wanted to stay at binary.com and no redirect
+            Cookies.set('binary-com-show', true, { expires: date });
+        } else {
+            window.location.replace('https://binary.bot/move-to-deriv');
+        }
+    
     };
 
     const loginOnLoad = () => {
